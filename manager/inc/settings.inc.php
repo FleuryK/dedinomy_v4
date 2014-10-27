136dda9326d7b4fa63c289324a1141ad17216d1c
Kévin <kevinfleury@laposte.net>
2014-10-27 16:27:45 +0100

    Mise à jour
    Mise à jour de cette page.

--- manager/inc/settings.inc.php
+++ manager/inc/settings.inc.php
@@ -17,16 +17,16 @@ if (isset($message)) echo '
 		<div class="five columns alpha">
 			<br />
 			<h4>Paramètres du script</h4>
-			<hr class="large" />
+			<hr class="large"/>
 			Vous pouvez modifier l'url d'accès à Dédinomy <strong>seulement, et seulement si</strong> vous êtes sûr de ce que vous faîtes. Nous avons détecté l'url suivante : <em style="color:grey">http://<?php echo $_SERVER["HTTP_HOST"].dirname(dirname($_SERVER["PHP_SELF"]));?></em>
 			<input style="margin-top:10px;" type="url" size="30" name="adr_scr" value="<?php if (isset($_POST['adr_scr'])) echo $_POST['adr_scr']; else echo $settings->adr_dedi;?>" required=required />
 			<strong>Nom de Votre Site :</strong>
 			<br />
-			<input type="text" size="30" name="site" value="<?php if (isset($_POST['site'])) echo $_POST['site']; else echo $settings->site;?>" required=required/>
+			<input type="text" size="30" name="site" value="<?php if (isset($_POST['site'])) echo $_POST['site']; else echo $settings->site;?>" required=required />
 			<div class="doc-section" id="mediaQueries"></div>
 			<br />
 			<h4>Formulaire d'envoi</h4>
-			<hr class="large" />
+			<hr class="large"/>
 			<fieldset>
 				<legend>Publication des nouvelles dédicaces</legend>
 				<label for="regularRadio">
@@ -51,27 +51,27 @@ if (isset($message)) echo '
 			</fieldset>
 			<strong>Caractères max de la dédicace</strong>
 			<br />
-			<input type="number" size="30" name="ca_post" value="<?php if(isset($_POST['ca_post'])) echo $_POST['ca_post']; else echo $settings->ca_post; ?>" required=required pattern="[0-9]{1,5}"/>
+			<input type="number" size="30" name="ca_post" value="<?php if(isset($_POST['ca_post'])) echo $_POST['ca_post']; else echo $settings->ca_post; ?>" required=required pattern="[0-9]{1,5}" />
 			<strong>Mots interdits séparés par une virgule</strong>
 			<br />
-			<input type="text" size="30" name="moderation" value="<?php if(isset($_POST['moderation'])) echo $_POST['moderation']; else echo $settings->moderation;?>" required=required/>
+			<input type="text" size="30" name="moderation" value="<?php if(isset($_POST['moderation'])) echo $_POST['moderation']; else echo $settings->moderation;?>" required=required />
 			<br />
 			<h4>Administration</h4>
 			<hr class="large"/>
 			<strong>Éléments affichées par page</strong><br />
-			<input type="text" size="30" name="adm_nb_page" value="<?php if (isset($_POST['adm_nb_page'])) echo $_POST['admin_nb_page']; else echo $settings->adm_nb_page;?>" required=required pattern="[0-9]{1,5}"/>
+			<input type="text" size="30" name="adm_nb_page" value="<?php if (isset($_POST['adm_nb_page'])) echo $_POST['admin_nb_page']; else echo $settings->adm_nb_page;?>" required=required pattern="[0-9]{1,5}" />
 		</div>
 		<div class="six columns omega">
 			<br />
 			<h4>Gestion Mail</h4>
-			<hr class="large" />
+			<hr class="large"/>
 			<strong>E-Mail de l'administrateur du site</strong><br />
-			<input type="email" size="30" name="mail_adm" value="<?php if(isset($_POST['mail_adm'])) echo $_POST['mail_adm']; else echo $settings->mail;?>" required=required/>
+			<input type="email" size="30" name="mail_adm" value="<?php if(isset($_POST['mail_adm'])) echo $_POST['mail_adm']; else echo $settings->mail;?>" required=required />
 			<div class="doc-section" id="mediaQueries"></div>
 			<br />
-			<h4>Affichage des dédicaces</h4><hr class="large" />
+			<h4>Affichage des dédicaces</h4><hr class="large"/>
 			<strong>Nombre de dédicaces affichées</strong><br />
-			<input type="text" size="30" name="nb_dedi" value="<?php if (isset($_POST['nb_dedi'])) echo $_POST['nb_dedi']; else echo $settings->nb_aff;?>" required=required pattern="[0-9]{1,5}"/>
+			<input type="text" size="30" name="nb_dedi" value="<?php if (isset($_POST['nb_dedi'])) echo $_POST['nb_dedi']; else echo $settings->nb_aff;?>" required=required pattern="[0-9]{1,5}" />
 			<fieldset>
 				<legend>Affichage de la date & heure à coté des dédicaces</legend>
 				<label for="regularRadio">
@@ -97,7 +97,7 @@ if (isset($message)) echo '
 					<tr class="alt">
 						<td>list_dedi</td>
 						<td><input type="radio" name="api_list_dedi" value="0" <?php if ($settings->api_list_dedi == 0) { echo 'checked'; }?> /></td>
-						<td><input type="radio" name="api_list_dedi" value="1" <?php if ($settings->api_list_dedi == 1) { echo 'checked'; }?>/></td>
+						<td><input type="radio" name="api_list_dedi" value="1" <?php if ($settings->api_list_dedi == 1) { echo 'checked'; }?> /></td>
 					</tr>
 					<tr>
 						<td>publish_dedi</td>
@@ -112,11 +112,11 @@ if (isset($message)) echo '
 				</tbody>
 			</table>    
 			<strong>L'API Dédinomy</strong> vous permet d'accéder aux dédicaces en dehors du script, et d'offrir à vos utilisateurs une autre expérience. Vous pouvez limiter l'accès aux informations - <strong>en mode public</strong>, c'est à dire ouvert, ou en <strong>mode privé</strong> nécessitant une clef API pour pouvoir obtenir les données. Pour plus d'informations, connectez-vous au forum NuBOX.
-			<strong>Votre clef API secrète :</strong> <input type="text" style="margin-top:5px; width:260px; " name="key" readonly="readonly" value="<?php echo $settings->api_key;?>"/>
+			<strong>Votre clef API secrète :</strong> <input type="text" style="margin-top:5px; width:260px; " name="key" readonly="readonly" value="<?php echo $settings->api_key;?>" />
 		</div>
 		<div class="eleven columns alpha">
 			<br />
-			<hr class="large" />
+			<hr class="large"/>
 			<h3>Messages Personnalisés</h3>
 			<ul class="tabs">
 				<li><a class="active" href="#Maintenance">Maintenance active</a></li>
@@ -150,5 +150,5 @@ if (isset($message)) echo '
 		<?php echo' 
 		<br /><br /><input type="hidden" name="id" value="1" />
 		<div style="top: 0px;position:fixed;background:#EBEBEB;width:700px;opacity:0.80;">
-			<button style="margin-left:10px;margin-top:10px;" class="button" type="submit">Enregistrer les paramètres</button>
+			<button style="margin-left:10px;margin-top:10px;" class="button" value="submit">Enregistrer les paramètres</button>
 		</form>';?>
